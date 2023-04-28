61d6d9fdd73c04ee2c3ba43573493e94a53dc805
Biittaa <123160799+Biittaa@users.noreply.github.com>
2023-04-28 20:08:06 +0430

    commit vote

---
+++ Vote.java
@@ -0,0 +1,10 @@
+import java.util.Objects;
+public class Vote {
+    private final Person voter;
+    private final String date;
+
+    public Vote(Person voter, String date) {
+        this.voter = voter;
+        this.date = date;
+    }
+}
