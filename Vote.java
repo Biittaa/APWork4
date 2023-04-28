362fa0640d0893559e539a83ef8f138269bef535
Biittaa <123160799+Biittaa@users.noreply.github.com>
2023-04-28 20:09:54 +0430

    commit vote getter

--- Vote.java
+++ Vote.java
@@ -7,4 +7,11 @@ public class Vote {
         this.voter = voter;
         this.date = date;
     }
+    public Person getVoter() {
+        return voter;
+    }
+    public String getDate() {
+        return date;
+    }
+
 }
